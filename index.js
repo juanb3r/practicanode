fb2893b1179e0c8b66dc11503a9603fce30087e8
juanber <jmesteman@hotmail.com>
2020-11-18 15:18:17 -0500

    Mi primera practica con nodejs. modulos y funciones matematicas

---
+++ index.js
@@ -0,0 +1,6 @@
+const math = require("./math.js");
+
+console.log(math);
+/*console.log(math.substract(3,2));
+console.log(math.multiply(3,2));
+console.log(math.divide(3,0));*/
---
+++ math.js
@@ -0,0 +1,30 @@
+
+var math = {};
+
+function add(x1, x2){
+    return x1 + x2;
+}
+
+function substract(x1, x2){
+    return x1 - x2;
+}
+
+function multiply(x1, x2){
+    return x1 * x2;
+}
+
+function divide(x1, x2){
+    if (x2==0) {
+        console.log("Error, no se puede dividir por cero")
+    }else{
+        return x1 / x2;
+    }
+    
+}
+
+math.add = add;
+math.substract = substract;
+math.multiply = multiply;
+math.divide = divide;
+
+module.exports= math;
